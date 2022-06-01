44483f18e0c0cdcf6a933a25449bf4e9dd10415d
webdevbiv <80968651+webdevbiv@users.noreply.github.com>
2022-06-01 18:56:24 +0300

    fix

--- src/components/FriendList/FriendList.js
+++ src/components/FriendList/FriendList.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+// import React, { Component } from "react";
 import React from "react";
 import PropTypes from "prop-types";
 import { Section, FriendListWrapper, FriendListList, FriendListItem, FriendListStatus, FriendListImage, FriendListName } from '../style'
