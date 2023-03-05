332a20ffbc66609ccd7b76ef233ac609df5f7383
webdevbiv <80968651+webdevbiv@users.noreply.github.com>
2023-03-05 18:49:28 +0200

    profile-done

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,33 +1,36 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import s from '../Profile/Profile.module.scss'
 
 function Profile({ username, tag, location, avatar, stats: { followers, views, likes } }) {
     return (
         <div className={`section`}>
-            <div className="description">
-                <img
-                    src={avatar}
-                    alt="User avatar"
-                    className="avatar"
-                />
-                <p className="name">{username}</p>
-                <p className="tag">{tag}</p>
-                <p className="location">{location}</p>
+            <div className={s.profile}>
+                <div className="description">
+                    <img
+                        src={avatar}
+                        alt="User avatar"
+                        className={s.avatar}
+                    />
+                    <p className={s.name}>{username}</p>
+                    <p className={s.tag}>{tag}</p>
+                    <p className={s.tag}>{location}</p>
+                </div>
+                <ul className={s.stats}>
+                    <li className={s.item}>
+                        <span className={s.label}>Followers</span>
+                        <span className={s.quantity}>{followers}</span>
+                    </li>
+                    <li className={s.item}>
+                        <span className={s.label}>Views</span>
+                        <span className={s.quantity}>{views}</span>
+                    </li>
+                    <li className={s.item}>
+                        <span className={s.label}>Likes</span>
+                        <span className={s.quantity}>{likes}</span>
+                    </li>
+                </ul>
             </div>
-            <ul className="stats">
-                <li>
-                    <span className="label">Followers</span>
-                    <span className="quantity">{followers}</span>
-                </li>
-                <li>
-                    <span className="label">Views</span>
-                    <span className="quantity">{views}</span>
-                </li>
-                <li>
-                    <span className="label">Likes</span>
-                    <span className="quantity">{likes}</span>
-                </li>
-            </ul>
         </div>
     )
 }
